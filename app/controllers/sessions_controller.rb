15344a1c3ddce623caf3d48377f537b6b5f68573
Alex Emrie <emrieaj@gmail.com>
2015-02-26 20:00:55 -0700

    User can sign-in and redirects to index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,20 +6,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(user_name: params[:user_name])
-    if @user && @user.authenticate(params[:user_name][:password])
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = @user.id
+      flash[:notice] = "Thanks for signing up!"
       redirect_to root_path
     else
-      @user = User.new(params[:user_name])
-      @user.errors[:base] << "Username / password is invalid"
-      render :new
+      render:new
     end
+
+
+
   end
 
   def destroy
     session.destroy
     redirect_to signin_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :password)
   end
 
 end
